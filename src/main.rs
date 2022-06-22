5346c9545d18e2ba68858ea5b0e43078a42a0d52
valkyrie_pilot <valk@randomairborne.dev>
2022-06-22 10:42:17 -0600

    Drop dependencies, increase robustness, and add more platforms

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-use std::io::Write;
+use std::{io::Write, path::PathBuf};
 
-#[tokio::main]
-async fn main() {
-    let mut time: u64 = tokio::fs::read_to_string("~/Library/tuimer/time.text")
-        .await
-        .unwrap()
+fn main() {
+    #[cfg(target_os = "macos")]
+    let place = std::env::var("HOME").expect("No HOME env var") + "/Library/tuimer/time.txt";
+    #[cfg(any(
+        target_os = "linux",
+        target_os = "freebsd",
+        target_os = "dragonfly",
+        target_os = "openbsd",
+        target_os = "netbsd"
+    ))]
+    let place = std::env::var("HOME").expect("No HOME env var") + "/.local/tuimer/time.txt";
+    #[cfg(target_os = "windows")]
+    let place = std::env::var("LocalAppData").expect("No LocalAppData env var") + "\\tuimer\\time.txt";
+    println!("{}", &place);
+    let path = PathBuf::from(place).canonicalize().expect("Failed to resolve path");
+    if !path.exists() {
+        std::fs::create_dir_all(&path.parent().unwrap()).expect("Failed to create directory");
+        std::fs::write(&path, "1").expect("Failed to create new file");
+    }
+    let mut time: u64 = std::fs::read_to_string(&path)
+        .expect("Failed to read file")
+        .trim()
         .parse()
-        .unwrap();
+        .expect("Failed to parse time as int");
     loop {
         let seconds = time % 60;
         let minutes = (time / 60) % 60;
@@ -14,11 +31,7 @@ async fn main() {
         print!("{:02}:{:02}:{:02}\r", hours, minutes, seconds,);
         std::io::stdout().flush().unwrap();
         time += 1;
-        tokio::spawn(async move {
-            tokio::fs::write("~/Library/tuimer/time.text", time.to_string())
-                .await
-                .ok();
-        });
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        std::fs::write(&path, time.to_string()).ok();
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
